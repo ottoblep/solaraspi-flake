32ffe87fb2f9f3bec036bfa8efc30abe0e525117
Severin Lochschmidt <seviron53@gmail.com>
2023-07-11 16:25:39 +0000

    Add homeassistant

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,35 @@
     };
   };
 
+  services.home-assistant = {
+    enable = true;
+    openFirewall = true;
+    config = {
+      homeassistant = {
+        name = "Solaraspi";
+        unit_system = "metric";
+        time_zone = "Europe/Berlin";
+      };
+
+      frontend = {
+        themes = "!include_dir_merge_named themes";
+      };
+
+      http = {};
+      feedreader.urls = [ "https://nixos.org/blogs.xml" ];
+    };
+    configWritable = true; # enable for testing settings
+    extraComponents = 
+    [
+      "analytics"
+      "default_config"
+      "mqtt"
+      "esphome"
+      "my"
+      "rpi_power"
+    ];
+  };
+
   users = let
     sevi-key = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDpZOwdinQQ8yxfbfe0fASggxkvOdC3dETtxcP2AGbY1DxdVX8EijGSvORN+FIf+JZlS9bvg48UWZOLYHVbWyVnv9M5bPgK8OKgjD/HT5oiIXtCJKtFveroZyc8L9cOadZRBGYoOQrRfMKAnUC1wp1aw5gSAIC5+JPrb+OjKoRwXYwRv+mtXQw+E6DO8nAsVZ8B7u+NyHwYF7uSR+Gl8hbaBriiGlSe0gqIxGNq7CafYZ9uR2wbVZRX8k+mga7gHogY1KaCUmagNG3jDd/d/NIobzO0FJBbPg5J01dGfSOg0EljAlxywLxCERwbtNakHAgsq9qenBgc4wIgiKz/LqmV Sevis SSH Key" ];
   in {
